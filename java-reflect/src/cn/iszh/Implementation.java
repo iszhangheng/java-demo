2d7717c605d5d65289fd03fca8ac1d8edc126c47
iszhangheng <iszhangheng@foxmail.com>
2019-11-05 07:36:41 +0800

    init

---
+++ java-reflect/src/cn/iszh/Implementation.java
@@ -0,0 +1,16 @@
+package cn.iszh;
+
+/**
+ * @version: V1.0
+ * @author: Mr.Zhang
+ * @className: Implementation
+ * @packageName: cn.iszh
+ * @description: 实现
+ * @date: 2019-11-04 21:49
+ **/
+public class Implementation implements SomeMethods {
+    @Override
+    public void interesting(String arg) {
+        System.out.println("interesting" + arg);
+    }
+}
